a1efdba7cab6b0ae88de222643b02908d7f3586b
Miłosz Głowaczewski <milosz.glowaczewski@aidlab.com>
2021-01-21 23:54:25 +0100

    Fixed issue with connecting to DB

---
+++ src/var/cache/prod/smarty/cache/ps_customtext/1/1/1/14/0e/85/55/0e8555404b3c08e72e79124765ac135a86dfd60c.pscustomtextpscustomtext..php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.33, created on 2021-01-21 22:44:31
+  from 'module:pscustomtextpscustomtext.' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_6009f5bf9640b5_52762185',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8654b2ba7ef103395c5eb0a4a12ed7463d291bc8' => 
+    array (
+      0 => 'module:pscustomtextpscustomtext.',
+      1 => 1532085910,
+      2 => 'module',
+    ),
+  ),
+  'cache_lifetime' => 31536000,
+),true)) {
+function content_6009f5bf9640b5_52762185 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div id="custom-text">
+  <h2>Najlepsze kursy w sieci</h2>
+<p>Kursy po Kowalsku</p>
+<p>Nasza firma otrzymała miano najlepszej platformy do nauki programowania</p>
+</div>
+<?php }
+}
